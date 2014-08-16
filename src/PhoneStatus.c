8de404dbe397d60ffd840987709f5493dca233bd
dareal68 <dareal68@gmail.com>
2014-08-16 04:16:23 +0200

    Update PhoneStatus.c

--- src/PhoneStatus.c
+++ src/PhoneStatus.c
@@ -18,12 +18,30 @@ void window_load(Window *window)
 {
     //Window's creation elements
     
+    text_layer = text_layer_create(GRect(0, 53, 132, 168));
+    text_layer_set_background_color(text_layer, GColorClear);
+    text_layer_set_text_color(text_layer, GColorBlack);
+    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+    text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
     
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+//    text_layer_set_text(text_layer, "Anything you want, as long as it is in quotes!");
+    
+    inv_layer = inverter_layer_create(GRect (0, 50, 144, 62));
+    layer_add_child(window_get_root_layer(window), (Layer*) inv_layer);
+	
+	struct tm *t;
+    time_t temp;
+    temp = time(NULL);
+    t = localtime(&temp);
+    tick_handler(t, MINUTE_UNIT);
 }
 
 void window_unload(Window *window)
 {
     //Window's destroy elements
+    text_layer_destroy(text_layer);
+    inverter_layer_destroy(inv_layer);
 }
 
 void init()
@@ -37,30 +55,11 @@ void init()
     window_stack_push(window, true);
     
     tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler) tick_handler);
-    struct tm *t;
-    time_t temp;
-    temp = time(NULL);
-    t = localtime(&temp);
-    tick_handler(t, MINUTE_UNIT);
-    
-    text_layer = text_layer_create(GRect(0, 53, 132, 168));
-    text_layer_set_background_color(text_layer, GColorClear);
-    text_layer_set_text_color(text_layer, GColorBlack);
-    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-    text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-    
-    layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
-    // text_layer_set_text(text_layer, "Anything you want, as long as it is in quotes!");   inutile pour afficher l'heure
-    
-    inv_layer = inverter_layer_create(GRect (0, 50, 144, 62));
-    layer_add_child(window_get_root_layer(window), (Layer*) inv_layer);
 }
 
 void deinit()
 {
     //De-initialization
-    text_layer_destroy(text_layer);
-    inverter_layer_destroy(inv_layer);
     window_destroy(window);
     tick_timer_service_unsubscribe();
 }
